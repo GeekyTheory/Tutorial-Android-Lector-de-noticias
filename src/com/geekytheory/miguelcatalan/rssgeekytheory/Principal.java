c13bdec0eda75a19b824457b7a8328e1367f61a3
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-03-28 20:54:56 +0100

    Tutorial 3 - Añadiendo la logica al Splash

---
+++ src/com/geekytheory/miguelcatalan/rssgeekytheory/Principal.java
@@ -0,0 +1,15 @@
+package com.geekytheory.miguelcatalan.rssgeekytheory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Principal extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+
+}
--- src/com/geekytheory/miguelcatalan/rssgeekytheory/Splash.java
+++ src/com/geekytheory/miguelcatalan/rssgeekytheory/Splash.java
@@ -4,22 +4,35 @@ import com.geekytheory.miguelcatalan.rssgeekytheory.R;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class Splash extends Activity {
 
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        lanzarThread();
+    }
+    
+    private void lanzarThread(){
+    	Thread timer = new Thread(){
+    		public void run(){
+    			try {
+					sleep(2000); //espera de 2 segundos
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}finally{
+					Intent intent = new Intent(Splash.this, Principal.class);
+					startActivity(intent);
+				}
+    		}
+    	};
+    	
+    	timer.start();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
-    }
     
 }
